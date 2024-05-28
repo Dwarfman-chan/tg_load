cd17e25d3388fce9c7ca2695624f2fe6ba205bef
Dwarfman-chan <gab.ittop@gmail.com>
2024-05-28 11:54:54 +0300

    фііфвфівфі

---
+++ json_data/test.py
@@ -0,0 +1,19 @@
+import json
+from datetime import datetime
+
+file_path = r'json_data\result.json'
+
+with open(file_path, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+messages = data["messages"]
+
+for message in messages:
+    date_str = message['date']
+    if date_str is not None:
+        dt = datetime.fromisoformat(date_str)
+        date_str = dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    text = message['text']
+
+    
